3ff2f6229df928cd739e4e307f1519ab8574c979
Ruwan <ruwanliyanagegalle1994@gmail.com>
2023-08-12 10:13:47 +0530

    Add basic methods

--- book/src/main/java/controller/BookController.java
+++ book/src/main/java/controller/BookController.java
@@ -1,4 +1,21 @@
 package controller;
 
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/book")
 public class BookController {
+    @Path("/create")
+    @POST
+    public Response createBook(){
+        return Response.ok("Create API working", MediaType.APPLICATION_JSON).build();
+    }
+
+    @Path("/book/{isbn}")
+    public Response getBookByISBN(@PathParam("isbn") String isbnNumber){
+        return Response.ok(isbnNumber, MediaType.APPLICATION_JSON).build();
+    }
 }
